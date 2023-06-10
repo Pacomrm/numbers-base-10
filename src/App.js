a6fbe78831effa860a75ee81e9448859a0fed13a
mrm <rodriguez.pacorro@gmail.com>
2023-06-09 18:53:24 -0600

    transformed number not working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
+
+    const [transformedNumber, setTransformedNumber] = useState('');
+    const [number, setNumber] = useState('');
+    const [base, setBase] = useState('');
+
+    const baseToChar = (base) => {
+        const baseChars = '0123456789abcdefghijklmnopqrstuvwxyz';
+        if (base === 10) {
+            return baseChars;
+        }
+        const baseLength = base.toString().length;
+        const baseMap = Array.from({ length: baseLength }, (_, i) => {
+            return i.toString();
+        });
+        return baseMap.join('');
+    };
+
+    const handleInputChange = (event) => {
+        console.log("here"+transformedNumber);
+        const { name, value } = event.target;
+
+        if (name === 'number') {
+            setNumber(value);
+        } else if (name === 'base') {
+            setBase(value);
+        }
+        let newBase = 0;
+
+        if (newBase === 0 || isNaN(newBase)) {
+            return;
+        }
+
+        const transformed = (num) => {
+            let result = '';
+            while (num > 0) {
+                const remainder = num % newBase;
+                result = baseToChar(newBase) + result;
+                num = Math.floor(num / newBase);
+            }
+            return result;
+        };
+
+        const transformedValue = transformed(parseInt(value, 10));
+        console.log(transformedValue);
+        setTransformedNumber(transformedValue);
+    };
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <main>
+        <div className="container-md">
+          <p>Transform a number Base 10 into any</p>
+          <p>Number to be used</p>
+          <input
+              type="number"
+              name="number"
+              value={number}
+              onChange={(e) => handleInputChange(e)}
+              data-number={number}
+          />
+          <p>Base to be transformed</p>
+          <input
+              type="number"
+              name="base"
+              value={base}
+              onChange={(e) => handleInputChange(e)}
+              data-base={base}
+          />
+        <p>Transformed Number: {transformedNumber}</p>
+        </div>
+      </main>
+      <footer></footer>
     </div>
   );
 }
